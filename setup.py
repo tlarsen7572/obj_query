f3e0f0333b1692b9fc8cd4db295898d037065b64
Tom Larsen <larsenthomasj@gmail.com>
2020-08-19 10:40:16 -0400

    update for publishin to pypi

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
    name='obj_query',
-   version='0.5',
+   version='0.6',
    description='translate JSON and Python objects to Alteryx RecordInfos',
    license="MIT",
    author='Thomas Larsen',
    author_email='larsenthomasj@gmail.com',
    packages=['obj_query'],
    install_requires=[],
+   url='https://github.com/tlarsen7572/obj_query',
 )
